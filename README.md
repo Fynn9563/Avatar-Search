bf70f3daf104355eac11019ff41ef433c06f9d9c
Riku9563 <68993521+Riku9563@users.noreply.github.com>
2023-07-18 12:04:47 +1000

    Added Files

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# Avatar-Search
+
+# Avatar Search
+
+Avatar Search is a web application built with Flask and SQLite that allows you to search for avatars and retrieve their information from a database. You can search by avatar name or ID, upload avatar information from JSON files, and retrieve avatar data in JSON format.
+
+## Features
+
+- Search avatars by name or ID
+- Upload avatar information from JSON files
+- Retrieve avatar data in JSON format
+
+## Prerequisites
+
+- Python 3.x
+- Flask
+- SQLite
+- Requests
+
+## Installation
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/Fynn9563/AvatarSearch.git` 
+```
+2.  Install the required dependencies:
+
+```bash
+`pip install -r requirements.txt` 
+```
+3.  Run the application:
+
+```bash
+`python server.py` 
+```
+The application will start running on [http://127.0.0.1:5000/](http://127.0.0.1:5000/).
+
+## Usage
+
+1.  Access the application in your web browser using the URL `http://127.0.0.1:5000/`.
+2.  Enter the avatar name or ID in the search box and click the **Search** button to retrieve matching avatars.
+3.  To upload avatar information from a JSON file, click the **Upload** button and select the JSON file.
+
+## VRCX Integration
+
+To integrate with VRCX and enable avatar searching from within VRCX, follow these steps:
+
+1.  Open VRCX and go to **Settings**.
+2.  Click on **Advanced**.
+3.  Click on **Avatar Database Provider**.
+4.  Add `http://127.0.0.1:5000/json` as the avatar database URL.
+
+Now you can search for avatars directly from VRCX using the Avatar Search functionality.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](https://github.com/Fynn9563/AvatarSearch/blob/master/LICENSE) file for more information.
---
+++ Server.py
@@ -0,0 +1,115 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+import sqlite3
+import os
+import json
+import tempfile
+
+app = Flask(__name__, template_folder='templates')
+DATABASE_FILE = 'avatars.db'
+
+# Function to create the Avatars table if it doesn't exist
+def create_table():
+    conn = sqlite3.connect(DATABASE_FILE)
+    cursor = conn.cursor()
+    cursor.execute('''DROP TABLE IF EXISTS Avatars''')
+    cursor.execute('''CREATE TABLE Avatars
+                      (id TEXT PRIMARY KEY,
+                       name TEXT,
+                       author TEXT)''')
+    conn.commit()
+    conn.close()
+
+# Function to search avatars by name or ID
+def search_avatars(search_term, limit=5000):
+    conn = sqlite3.connect(DATABASE_FILE)
+    cursor = conn.cursor()
+
+    if search_term:
+        query = "SELECT * FROM Avatars WHERE name LIKE ? OR id = ? LIMIT ?"
+        cursor.execute(query, (f'%{search_term}%', search_term, limit))
+    else:
+        query = "SELECT * FROM Avatars LIMIT ?"
+        cursor.execute(query, (limit,))
+
+    results = cursor.fetchall()
+    conn.close()
+    return results
+    
+# Function to import avatars from a JSON file
+def import_avatars_from_json(json_file):
+    with open(json_file, 'r') as file:
+        avatars_data = json.load(file)
+
+    conn = sqlite3.connect(DATABASE_FILE)
+    cursor = conn.cursor()
+
+    for avatar in avatars_data:
+        avatar_id = avatar['id']
+        avatar_name = avatar['name']
+        avatar_author = avatar['author']
+
+        # Check if avatar with the same ID already exists
+        query = "SELECT id FROM Avatars WHERE id = ?"
+        cursor.execute(query, (avatar_id,))
+        existing_avatar = cursor.fetchone()
+
+        if existing_avatar:
+            # Overwrite the existing avatar record
+            query = "UPDATE Avatars SET name = ?, author = ? WHERE id = ?"
+            cursor.execute(query, (avatar_name, avatar_author, avatar_id))
+        else:
+            # Insert the new avatar record
+            query = "INSERT INTO Avatars (id, name, author) VALUES (?, ?, ?)"
+            cursor.execute(query, (avatar_id, avatar_name, avatar_author))
+
+    conn.commit()
+    conn.close()
+
+# Route for file upload
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            with tempfile.NamedTemporaryFile(delete=False) as temp:
+                file.save(temp.name)
+                import_avatars_from_json(temp.name)
+            return redirect(url_for('home'))
+    return render_template('upload.html')
+
+@app.route('/json', methods=['GET'])
+def json_output():
+    search_term = request.args.get('search')
+    n = request.args.get('n', type=int, default=5000)
+
+    results = search_avatars(search_term, limit=n)
+
+    avatars = []
+    for row in results:
+        avatar = {
+            'id': row[0],
+            'name': row[1],
+            'author': row[2]
+        }
+        avatars.append(avatar)
+
+    return jsonify(avatars)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        search_term = request.form['search']
+        results = search_avatars(search_term)
+        if len(results) == 0:
+            message = "No avatars found matching the search criteria."
+        else:
+            message = None
+        return render_template('index.html', results=results, message=message)
+    else:
+        return render_template('index.html')
+
+if __name__ == '__main__':
+    if not os.path.exists(DATABASE_FILE):
+        create_table()
+
+    app.run()
